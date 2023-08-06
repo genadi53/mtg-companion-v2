5274649512da632b5309cd1f39abc287e2075c73
genadi53 <kotka5353@gmail.com>
2023-08-06 20:01:54 +0300

    added card types

--- src/utils/fetchTypes.ts
+++ src/utils/fetchTypes.ts
@@ -38,6 +38,147 @@ export interface MTG_Symbol {
   gatherer_alternates: string[] | null;
 }
 
+export interface Card {
+  object: string;
+  id: string;
+  lang: string;
+  mtgo_id?: number | null;
+  oracle_id: string;
+  multiverse_ids?: number[] | null;
+  arena_id?: number | null;
+  tcgplayer_id?: number | null;
+  cardmarket_id?: number | null;
+  prints_search_uri: string;
+  rulings_uri: string;
+  scryfall_uri: string;
+  uri: string;
+  name: string;
+
+  all_parts?: RelatedCards[] | null;
+  card_faces?: CardFacesDetails[];
+  cmc: number;
+  color_identity: string[];
+  color_indicator?: string[];
+  colors?: string[];
+  keywords: string[];
+  layout: string;
+  legalities: Record<string, Legalities>;
+  loyalty?: string;
+  mana_cost?: string;
+  oracle_text?: string;
+  oversized: boolean;
+  power?: string;
+  produced_mana?: string[];
+  reserved: boolean;
+  type_line: string;
+  toughness?: string;
+
+  artist: string;
+  attraction_lights?: [];
+  booster: boolean;
+  border_color: "black" | "white" | "borderless" | "silver" | "gold";
+  card_back_id: string;
+  collector_number: string;
+  content_warning?: boolean;
+  digital: boolean;
+  finishes: string[];
+  flavor_name?: string;
+  frame_effects?: [];
+  frame: string;
+  full_art: boolean;
+  games: string[];
+  highres_image: boolean;
+  illustration_id?: string;
+  image_status: "missing" | "placeholder" | "lowres" | "highres_scan";
+  image_uris?: ImageTypes;
+  prices: CardPrices;
+  printed_name?: string;
+  printed_text?: string;
+  printed_type_line?: string;
+  promo: boolean;
+  promo_types: [];
+  purchase_uris: Record<string, string>;
+  rarity: Rarity;
+  related_uris: Record<string, string>;
+  released_at: string;
+  reprint: boolean;
+  scryfall_set_uri: string;
+  set_name: string;
+  set_id: string;
+  set: string;
+  set_type: string;
+  set_uri: string;
+  set_search_uri: string;
+  story_spotlight: boolean;
+  textless: boolean;
+  variation: boolean;
+  variation_of?: string;
+  foil: boolean;
+  nonfoil: boolean;
+  artist_ids: string[];
+  security_stamp?: string;
+  watermark?: string;
+  edhrec_rank?: number;
+  penny_rank?: number;
+  preview: CardPreview;
+}
+
+export interface RelatedCards {
+  object: string;
+  id: string;
+  component: string;
+  name: string;
+  type_line: string;
+  uri: string;
+}
+
+export interface CardFacesDetails {
+  artist: string;
+  cmc: number;
+  object: string;
+  name: string;
+  mana_cost: string;
+  type_line: string;
+  oracle_text: string;
+  colors: string[];
+  artist_id: string;
+  illustration_id: string;
+  image_uris: ImageTypes;
+}
+
+export interface ImageTypes {
+  small: string;
+  normal: string;
+  large: string;
+  png: string;
+  art_crop?: string;
+  border_crop?: string;
+}
+
+export type Legalities = "legal" | "not_legal" | "restricted" | "banned";
+export type Rarity =
+  | "common"
+  | "uncommon"
+  | "rare"
+  | "special"
+  | "mythic"
+  | "bonus";
+
+export interface CardPrices {
+  usd: string;
+  usd_foil: string | null;
+  usd_etched: string | null;
+  eur: string;
+  eur_foil: string | null;
+  tix: string;
+}
+
+export interface CardPreview {
+  source: string;
+  source_uri: string;
+  previewed_at: string;
+}
+
 export interface GenericFetchResponce {
   object: string;
   has_more?: boolean;
@@ -52,4 +193,8 @@ export interface FetchSetsResponce extends GenericFetchResponce {
 
 export interface FetchSymbolsResponce extends GenericFetchResponce {
   data: MTG_Symbol[];
+}
+
+export interface FetchCardsResponce extends GenericFetchResponce {
+  data: Card[];
 }
